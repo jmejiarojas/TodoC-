fdfab4ea9325cd56fb7776f1a7e9cdda7fe249fb
jmejiarojas <julioedgarmejiarojas338@gmail.com>
2016-03-25 14:18:04 -0500

    Crea el Fomulario del proyecto
    -Se crea la interfaz grafica, además se intancia a la clase Pago, se crea métodos para cada una de las operaciones.
    -Se hace uso de mucha refactorizacion para hacer un código mas limpio y entendible.

--- PagoVendedores/frmPrincipal.Designer.cs
+++ PagoVendedores/frmPrincipal.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PagoVendedores
 {
-    partial class Form1
+    partial class frmPrincipal
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtVendedor = new System.Windows.Forms.TextBox();
+            this.txtHoras = new System.Windows.Forms.TextBox();
+            this.cboCategoria = new System.Windows.Forms.ComboBox();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.lvR = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(108, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(285, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CONTROL DE PAGO DE VENDEDORES";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "VENDEDOR";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "CATEGORIA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(213, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "HORAS DE TRABAJO";
+            // 
+            // txtVendedor
+            // 
+            this.txtVendedor.Location = new System.Drawing.Point(111, 52);
+            this.txtVendedor.Name = "txtVendedor";
+            this.txtVendedor.Size = new System.Drawing.Size(377, 20);
+            this.txtVendedor.TabIndex = 1;
+            // 
+            // txtHoras
+            // 
+            this.txtHoras.Location = new System.Drawing.Point(352, 86);
+            this.txtHoras.Name = "txtHoras";
+            this.txtHoras.Size = new System.Drawing.Size(136, 20);
+            this.txtHoras.TabIndex = 1;
+            // 
+            // cboCategoria
+            // 
+            this.cboCategoria.FormattingEnabled = true;
+            this.cboCategoria.Location = new System.Drawing.Point(111, 83);
+            this.cboCategoria.Name = "cboCategoria";
+            this.cboCategoria.Size = new System.Drawing.Size(77, 21);
+            this.cboCategoria.TabIndex = 2;
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.Location = new System.Drawing.Point(36, 133);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(75, 23);
+            this.btnProcesar.TabIndex = 3;
+            this.btnProcesar.Text = "PROCESAR";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(145, 133);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 3;
+            this.btnLimpiar.Text = "LIMPIAR";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(36, 324);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 3;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // lvR
+            // 
+            this.lvR.Location = new System.Drawing.Point(36, 180);
+            this.lvR.Name = "lvR";
+            this.lvR.Size = new System.Drawing.Size(452, 130);
+            this.lvR.TabIndex = 4;
+            this.lvR.UseCompatibleStateImageBehavior = false;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 359);
+            this.Controls.Add(this.lvR);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnProcesar);
+            this.Controls.Add(this.cboCategoria);
+            this.Controls.Add(this.txtHoras);
+            this.Controls.Add(this.txtVendedor);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmPrincipal";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtVendedor;
+        private System.Windows.Forms.TextBox txtHoras;
+        private System.Windows.Forms.ComboBox cboCategoria;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.ListView lvR;
     }
 }
 
--- PagoVendedores/frmPrincipal.cs
+++ PagoVendedores/frmPrincipal.cs
@@ -10,11 +10,129 @@ using System.Windows.Forms;
 
 namespace PagoVendedores
 {
-    public partial class Form1 : Form
+    public partial class frmPrincipal : Form
     {
-        public Form1()
+        int n = 0;
+        double subtotal;
+
+        public frmPrincipal()
         {
+
             InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            cargarCombo();
+            configurarLista();
+        }
+
+        private void configurarLista()
+        {
+            lvR.View = View.Details;
+            lvR.GridLines = true;
+            lvR.Columns.Add("Nro",30);
+            lvR.Columns.Add("Vendedor", 120);
+            lvR.Columns.Add("Cat",40);
+            lvR.Columns.Add("Horas Tr.", 70);
+            lvR.Columns.Add("S. Bruto", 70);
+            lvR.Columns.Add("Descuento", 70);
+            lvR.Columns.Add("S. Neto", 70);
+        }
+
+        private void cargarCombo()
+        {
+            cboCategoria.Items.Add("A");
+            cboCategoria.Items.Add("B");
+            cboCategoria.Items.Add("C");
+            cboCategoria.Items.Add("D");
+        }
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                n++;
+                Pago pago = new Pago();
+                pago.vendedor = txtVendedor.Text;
+
+                leerHoras(pago);
+
+                pago.categoria = cboCategoria.Text;
+
+                //Aumentamos el subtotal que es una variable global
+                subtotal += pago.calcularSubtotal();
+
+                //Imprimiendo en el Listview
+                ListViewItem fila = new ListViewItem(n.ToString());
+                fila.SubItems.Add(pago.vendedor);
+                fila.SubItems.Add(pago.categoria);
+                fila.SubItems.Add(pago.horas.ToString());
+                fila.SubItems.Add(pago.calcularSubtotal().ToString());
+                fila.SubItems.Add(pago.calcularDescuento().ToString());
+                fila.SubItems.Add(pago.calcularNeto().ToString());
+                lvR.Items.Add(fila);
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Error en la aplicación !!", "Adevertencia",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void leerHoras(Pago pago)
+        {
+            if (String.IsNullOrEmpty(txtHoras.Text))
+            {
+                pago.horas = 30;
+            }
+            else
+            {
+                pago.horas = int.Parse(txtHoras.Text);
+            }
+        }
+
+        public void limpiarCajas()
+        {
+            txtVendedor.Clear();
+            txtHoras.Clear();
+            cboCategoria.SelectedIndex = -1;
+
+        }
+
+        public void reinicar()
+        {
+            n = 0;
+            subtotal = 0;
+            lvR.Items.Clear();
+            limpiarCajas();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            reinicar();
+        }
+
+        private void salir()
+        {
+            DialogResult r = MessageBox.Show("¿Está seguro de salir?", "Salir",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+            if(r == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            salir();
         }
     }
 }
