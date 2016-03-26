0c570aee457e9d1c756d1512248d4e3a2fba696c
jmejiarojas <julioedgarmejiarojas338@gmail.com>
2016-03-25 20:34:11 -0500

    Crea clase Padre Empleado

---
+++ PagoSueldosHerencia/Empleado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PagoSueldosHerencia
+{
+    class Empleado
+    {
+        public String codigo { get; set; }
+        public String nombres { get; set; }
+        public DateTime fechaIngreso { get; set; }
+        public String telefono { get; set; }
+
+        public int calculaAniosServicios()
+        {
+            return DateTime.Today.Year - fechaIngreso.Year;
+        }
+    }
+}
