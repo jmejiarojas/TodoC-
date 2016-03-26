c4d89f84d26c0fe93d2da7c2ad433159b2475e31
jmejiarojas <julioedgarmejiarojas338@gmail.com>
2016-03-25 20:38:19 -0500

    Crea Clase hija Asalariado

---
+++ PagoSueldosHerencia/Asalariado.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PagoSueldosHerencia
+{
+    class Asalariado:Empleado
+    {
+        public double bruto { get; set; }
+
+        public Asalariado()
+        {
+            bruto = 1200;
+        }
+
+        public double calculaDescuento()
+        {
+            return bruto * 0.1;
+        }
+
+        public double calcularNeto()
+        {
+            return bruto - calculaDescuento();
+        }
+    }
+}
