99db7d19c4e61013d309140ce8ca6ecbb2d7dbba
jmejiarojas <julioedgarmejiarojas338@gmail.com>
2016-03-25 21:13:14 -0500

    Agrega la GUI para Empleados Asalariados

--- PagoSueldosHerencia/frmPrincipal.Designer.cs
+++ PagoSueldosHerencia/frmPrincipal.Designer.cs
@@ -28,20 +28,198 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dtFecha = new System.Windows.Forms.DateTimePicker();
+            this.lvR = new System.Windows.Forms.ListView();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnRegistrar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(189, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "CODIGO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "NOMBRES";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "label1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(144, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(202, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "PAGO DE EMPELADOS ASALARIADOS";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 127);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "TELEFONO";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(189, 57);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(112, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "FECHA DE INGRESO";
+            // 
+            // dtFecha
+            // 
+            this.dtFecha.Location = new System.Drawing.Point(323, 51);
+            this.dtFecha.Name = "dtFecha";
+            this.dtFecha.Size = new System.Drawing.Size(200, 20);
+            this.dtFecha.TabIndex = 1;
+            // 
+            // lvR
+            // 
+            this.lvR.Location = new System.Drawing.Point(25, 163);
+            this.lvR.Name = "lvR";
+            this.lvR.Size = new System.Drawing.Size(497, 153);
+            this.lvR.TabIndex = 2;
+            this.lvR.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(25, 322);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 37);
+            this.btnSalir.TabIndex = 3;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.AcceptsReturn = true;
+            this.txtCodigo.Location = new System.Drawing.Point(90, 54);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(84, 20);
+            this.txtCodigo.TabIndex = 5;
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.Location = new System.Drawing.Point(89, 85);
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(280, 20);
+            this.txtNombres.TabIndex = 5;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(89, 120);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(169, 20);
+            this.txtTelefono.TabIndex = 5;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(337, 115);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(83, 37);
+            this.btnLimpiar.TabIndex = 3;
+            this.btnLimpiar.Text = "LIMPIAR";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(430, 115);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(83, 37);
+            this.btnRegistrar.TabIndex = 3;
+            this.btnRegistrar.Text = "REGISTRAR";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // frmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(497, 355);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(545, 371);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtNombres);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.lvR);
+            this.Controls.Add(this.dtFecha);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmPrincipal";
+            this.Text = "HERENCIA";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DateTimePicker dtFecha;
+        private System.Windows.Forms.ListView lvR;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtNombres;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnRegistrar;
     }
 }
 
--- PagoSueldosHerencia/frmPrincipal.cs
+++ PagoSueldosHerencia/frmPrincipal.cs
@@ -12,9 +12,86 @@ namespace PagoSueldosHerencia
 {
     public partial class frmPrincipal : Form
     {
+        int n = 0;
+
         public frmPrincipal()
         {
             InitializeComponent();
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            configurarLista();
+            txtCodigo.Enabled = false;
+            generarCodigo();
+        }
+
+        private void configurarLista()
+        {
+            lvR.View = View.Details;
+            lvR.GridLines = true;
+            lvR.Columns.Add("Código", 70);
+            lvR.Columns.Add("Nombres", 120);
+            lvR.Columns.Add("Ingreso", 80);
+            lvR.Columns.Add("Teléfono", 80);
+            lvR.Columns.Add("Descuento", 70);
+            lvR.Columns.Add("Neto", 80);
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            Asalariado objSal = new Asalariado();
+
+            //Obtener los valores del formulario
+            objSal.codigo = txtCodigo.Text;
+            objSal.nombres = txtNombres.Text;
+            objSal.fechaIngreso = dtFecha.Value;
+            objSal.telefono = txtTelefono.Text;
+
+            //Confirmacion para registrar
+            DialogResult r = MessageBox.Show("Está seguro de salir?", "Salir", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if(r == DialogResult.Yes)
+            {
+                //Enviamos los valores a la lista
+                ListViewItem fila = new ListViewItem(objSal.codigo);
+                fila.SubItems.Add(objSal.nombres);
+                fila.SubItems.Add(objSal.fechaIngreso.ToString("d"));
+                fila.SubItems.Add(objSal.telefono);
+                fila.SubItems.Add(objSal.calculaDescuento().ToString("0.00"));
+                fila.SubItems.Add(objSal.calcularNeto().ToString("0.00"));
+                lvR.Items.Add(fila);
+
+                generarCodigo();
+                limpiarControles();
+            }
+        }
+
+        private void limpiarControles()
+        {
+            txtNombres.Clear();
+            txtTelefono.Clear();
+            txtNombres.Focus();
+        }
+
+        private void generarCodigo()
+        {
+            n++;
+            txtCodigo.Text = "T" + n.ToString("0000");
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            limpiarControles();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult r = MessageBox.Show("Esta seguro de salir..!!",
+             "Salir",
+             MessageBoxButtons.YesNo,
+            MessageBoxIcon.Error);
+            if (r == DialogResult.Yes)
+                this.Close();
         }
     }
 }
