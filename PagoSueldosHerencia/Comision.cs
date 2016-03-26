610dd379a14197a2261de54430e3be81c58a15e5
jmejiarojas <julioedgarmejiarojas338@gmail.com>
2016-03-25 20:43:32 -0500

    Se agrega clase hija Comision

---
+++ PagoSueldosHerencia/Comision.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PagoSueldosHerencia
+{
+    class Comision:Empleado
+    {
+        public double montoVendido { get; set; }
+
+        public double calculaSueldo()
+        {
+            double sueldo = montoVendido * 0.1;
+            if (calculaAniosServicios() < 3)
+            {
+                return sueldo + 200;
+            }
+            else
+            {
+                return sueldo;
+            }
+        }
+    }
+}
